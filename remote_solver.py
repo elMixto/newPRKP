75c2fe3599a48bb7b2c590d74d41a8ee8683759c
Jose Gonzalez <joseco1577@gmail.com>
2023-08-03 18:07:52 -0400

    remote_solver_app, solves optimaly

--- remote_solver.py
+++ remote_solver.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask,request
 from asgiref.wsgi import WsgiToAsgi
 from flask import jsonify
-from lib.gurobi_solver import solve_polynomial_knapsack
+from lib.gurobi_solver import solve_polynomial_knapsack,VAR_TYPE
+from lib.Instance import Instance
 
 app = Flask(__name__)
 
 @app.route("/",methods = ["POST"])
 def solve():
-    return jsonify({"data":"Hola"})
-
+    data = request.json
+    instance = Instance.from_file(data)    
+    return jsonify(solve_polynomial_knapsack(instance,VAR_TYPE.BINARY,False,[]))
 
 asgiapp = WsgiToAsgi(app)
