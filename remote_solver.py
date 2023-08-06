955ef6c3a3be94e4b6abbe4e2fe972028a51348d
mixto <joseco1577@gmail.com>
2023-08-06 17:55:11 -0400

    Fixed

--- remote_solver.py
+++ remote_solver.py
@@ -3,14 +3,14 @@ from asgiref.wsgi import WsgiToAsgi
 from flask import jsonify
 from src.solvers.gurobi import solve_polynomial_knapsack,VAR_TYPE,SolverConfig
 from src.data_structures import Instance
-
+import json
 app = Flask(__name__)
 
 @app.route("/",methods = ["POST"])
 def solve():
     data = request.json
-    instance = Instance.from_dict(data['instance'])
-    solver_config = SolverConfig.from_dict(instance['solver_config'])
+    instance = Instance.from_dict(json.loads(data['instance']))
+    solver_config = SolverConfig.from_json(data['solver_config'])
     return jsonify(solve_polynomial_knapsack(instance,solver_config))
 
 asgiapp = WsgiToAsgi(app)
